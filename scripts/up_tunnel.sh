f31f9637ba397a901fd07ea932599e571eda391b
Ilya Shamovsky <ilya.shamovsky@nyumc.org>
2017-05-15 13:59:25 -0400

    Added autossh script

---
+++ scripts/up_tunnel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+tabs 4
+
+readonly HOST="lab.nudlerlab.info"
+readonly JPORTLOCAL=8988
+readonly JPORTREMOTE=8999
+readonly TUNPORT=8878
+readonly USER=$(whoami)
+
+autossh -R ${JPORTREMOTE}:localhost:${JPORTLOCAL} $HOST -N &
+autossh -fN -R ${TUNPORT}:localhost:22 "${USER}@${HOST}"
