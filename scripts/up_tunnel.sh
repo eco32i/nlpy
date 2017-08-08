21b90e886e1dd96404eb915a37664c0569e2438a
Ilya Shamovsky <ilya.shamovsky@nyumc.org>
2017-08-08 15:47:54 -0400

    systemd services for autossh tunnels to vps; tweaks to up_tunnel.sh script.

--- scripts/up_tunnel.sh
+++ scripts/up_tunnel.sh
@@ -9,5 +9,5 @@ readonly JPORTREMOTE=8999
 readonly TUNPORT=8878
 readonly USER=$(whoami)
 
-autossh -R ${JPORTREMOTE}:localhost:${JPORTLOCAL} $HOST -N &
+autossh -R ${JPORTREMOTE}:localhost:${JPORTLOCAL} "${USER}@${HOST}" -N &
 autossh -fN -R ${TUNPORT}:localhost:22 "${USER}@${HOST}"
