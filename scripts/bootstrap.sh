3000c68dc24ec98c62d338f1510df7a5b28998b5
Ilya Shamovsky <ilya.shamovsky@gmail.com>
2018-06-05 14:58:05 -0400

    Various changes to bootstrap.sh to make it work with bionic

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -14,12 +14,12 @@ install_core() {
     sudo apt install -y byobu htop vim vim-nox fonts-inconsolata openssh-server gtk2-engines-murrine \
         libcurl4-openssl-dev python-dev python3-dev build-essential cmake git linux-headers-generic \
         trimmomatic r-base libhdf5-100 hdf5-tools papirus-icon-theme arc-theme \
-        libopenblas-base libopenblas-dev gfortran g++ python-pip \
+        libopenblas-base libopenblas-dev gfortran g++ python-pip fonts-cantarell \
         samtools bedtools libpng-dev libjpeg8-dev libfreetype6-dev libxft-dev \
-        libhdf5-dev libatlas3-base libatlas-base-dev python3-venv libxml2-dev libxslt1-dev
+        tsocks libhdf5-dev libatlas3-base libatlas-base-dev python3-venv libxml2-dev libxslt1-dev
     sudo apt upgrade -y && sudo apt dist-upgrade -y
-    sudo update-alternatives --set libblas.so.3 /usr/lib/openblas-base/libblas.so.3
-    sudo update-alternatives --set liblapack.so.3 /usr/lib/openblas-base/liblapack.so.3
+    #sudo update-alternatives --set libblas.so.3 /usr/lib/openblas-base/libblas.so.3
+    #sudo update-alternatives --set liblapack.so.3 /usr/lib/openblas-base/liblapack.so.3
 }
 
 install_google() {
@@ -46,7 +46,7 @@ setup_env() {
         rm -rf $VENV_DIR/pydata3
     fi
 
-    pyvenv $VENV_DIR/pydata3
+    python3 -m venv $VENV_DIR/pydata3
     source $VENV_DIR/pydata3/bin/activate
     pip install -U pip
     cat $pydata | xargs -n 1 -L 1 pip install
@@ -59,13 +59,11 @@ setup_i3() {
     local locker="lock.sh"
     /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2018.01.30_all.deb keyring.deb SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
     sudo dpkg -i ./keyring.deb
-    disecho "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+    rm -rf ./keyring.deb
+    echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
 
-    #echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" | sudo tee -a /etc/apt/sources.list > /dev/null
     sudo apt update
-    # sudo apt-get install --allow-unauthenticated sur5r-keyring
-    # sudo apt update
-    sudo apt install i3 compton imagemagick scrot nitrogen
+    sudo apt install i3 imagemagick scrot nitrogen
     old_dir=$(pwd)
     cd $dir && cd ..
     if [ ! -d "$HOME/.i3" ]
