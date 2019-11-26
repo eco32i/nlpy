e8ebe25ac43b7af63e8471702efbb8c1daba3b85
Ilya Shamovsky <ilya.shamovsky@nyumc.org>
2019-11-26 13:27:48 -0500

    Added services dir, fixed keyring for i3 repos in bootstrap.sh, removed old files

--- utils/kbd_backlight_increase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-val=$(cat /sys/class/leds/asus::kbd_backlight/brightness)
-if [ 3 -gt $val  ]
-then
-    val=$(($val + 1))
-    echo $val | tee /sys/class/leds/asus::kbd_backlight/brightness
-fi
