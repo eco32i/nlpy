8da65ddf6a849bc75c6f555e3cbf11fb4f33f17a
Ilya Shamovsky <ilya.shamovsky@gmail.com>
2021-04-13 16:04:10 -0400

    Added inkscape and ninja-build to system packages

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -13,7 +13,7 @@ install_core() {
     sudo apt update
     sudo apt install -y byobu htop vim vim-nox fonts-inconsolata openssh-server gtk2-engines-murrine \
         libcurl4-openssl-dev python-dev python3-dev build-essential cmake git linux-headers-generic \
-        trimmomatic r-base libhdf5-103 hdf5-tools \
+        trimmomatic r-base libhdf5-103 hdf5-tools ninja-build inkscape \
         libopenblas-base libopenblas-dev gfortran g++ python3-pip fonts-cantarell \
         samtools bedtools libpng-dev libjpeg8-dev libfreetype6-dev libxft-dev \
         tsocks libhdf5-dev libatlas3-base libatlas-base-dev python3-venv libxml2-dev libxslt1-dev
